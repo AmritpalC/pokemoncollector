7ced3865beafdad38d6826c67aafffaa8863dd3d
AmritpalC <amritpalchahal@hotmail.co.uk>
2023-06-14 15:22:02 +0100

    app structure, templates, index and about views, some styling

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+pokemon = [
+    {'name': 'Bulbasaur', 'number': 1, 'weight': '6.9kg', 'height': '0.7m', 'type': 'Grass/Poison'},
+    {'name': 'Charmander', 'number': 4, 'weight': '8.5kg', 'height': '0.6m', 'type': 'Fire'},
+    {'name': 'Squirtle', 'number': 7, 'weight': '9.0kg', 'height': '0.5m', 'type': 'Water'},
+    {'name': 'Pikachu', 'number': 25, 'weight': '6.0kg', 'height': '0.4m', 'type': 'Electric'},
+]
+
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def allpokemon_index(request):
+    return render(request, 'allpokemon/index.html', {
+        'allpokemon': pokemon
+    })
--- pokemoncollector/settings.py
+++ pokemoncollector/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dzjq*2tskz7vkhx^8mqm73%07-iea34a@30=q#na7)g*i1zh-g'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,7 +81,11 @@ WSGI_APPLICATION = 'pokemoncollector.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'pokemoncollector',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': '5432',
     }
 }
 
